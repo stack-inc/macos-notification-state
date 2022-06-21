47f41e3aeebeb261b49d617218b6fa9c0527d78c
Yagiz Nizipli <yagiz@nizipli.com>
2022-06-21 10:14:17 -0400

    chore: add prefix to package name

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
-declare module 'macos-notification-state' {
+declare module '@stack-inc/macos-notification-state' {
   export function getNotificationState(): 'UNKNOWN_ERROR' | SessionState | 'DO_NOT_DISTURB';
-  export function getSessionState():  SessionState;
+  export function getSessionState(): SessionState;
   export function getDoNotDisturb(): boolean;
   export type SessionState = 'SESSION_SCREEN_IS_LOCKED' | 'SESSION_ON_CONSOLE_KEY' | 'UNKNOWN';
 }
